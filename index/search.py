afe5fb208d744e4a544e40e0485b9e595eee29b6
peterhn <peternguyen@Peters-Macbook-Pro.local>
2015-05-27 18:20:36 -0700

    improved algorithm, can now search for two words

--- index/search.py
+++ index/search.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+import json
+import os
 
 FILE_DOC_IDS = "./docIds.txt"
 FILE_INVERTED_INDEX = "./invertedIndex.txt"
 FILE_TERM_IDS = "./termIds.txt"
+FILE_DUMP = "./FileDump/"
+
 
 def _input(msg):
 
@@ -59,6 +63,19 @@ def parseTermIds():
 
     return termIds
 
+def parseDocs():
+    docTexts = {}
+    for fn in os.listdir(FILE_DUMP):
+        try:
+            with open(FILE_DUMP + fn) as data_file:
+                data = json.load(data_file)
+                docId = int(data["id"])
+                text = str(data["text"]).lower()
+                docTexts[docId] = text
+        except ValueError:
+            print('No valid JSON')
+    return docTexts
+
 def parseDocIds():
 
     # TermIds have the int ID as the key, followed by the string term as val
@@ -75,23 +92,47 @@ def parseDocIds():
 
     return docIds
 
-def handleSearchQuery(query, termIds, docIds, index):
+def handleSearchQuery(query, termIds, docIds, index, docTexts):
     # Searches for the query in the index
 
     # ONLY HANDLES SINGLE WORDS AT THE MOMENT
     # First get the term number from the query
-    if query not in termIds:
-        print("Failed to find " + query + " in list of terms")
-    else:
-        termId = termIds[query]
-        pages = index[termId]
-        genexp = ((k, pages[k]) for k in sorted(pages, key=pages.get, reverse=True)[:5])
 
-        print("The term: \"" + query +"\" occurs in the following documents...")
-        for k, v in genexp:
-            # k is docId
-            # v is frequency of occurance
-            print("\t", docIds[k], "\tOccurs:", v, "times")
+    seenDocIds = []
+    relevantDocIds = {}
+    search = query.split()
+    for word in search:
+        if word not in termIds:
+            print("Failed to find " + word + " in list of terms")
+        else:
+            termId = termIds[word]
+            pages = index[termId]
+            genexp = ((k, pages[k]) for k in sorted(pages, key=pages.get, reverse=True))
+
+            for k, v in genexp:
+                # k is docId
+                # v is frequency of occurance (will be used to calculate relevance)
+                if k not in seenDocIds:
+                    seenDocIds.append(k)
+                if k in seenDocIds:
+                    #print("seen", k)
+                    if k in relevantDocIds:
+                        #print("words")
+                        relevantDocIds[k] = relevantDocIds[k] + v
+                    else:
+                        relevantDocIds[k] = v
+
+    print("The term: \"" + query +"\" occurs in the following documents...")
+    for k in relevantDocIds:
+        docText = docTexts[k]
+        exactQueryCount = docText.count(query)
+        relevantDocIds[k] = relevantDocIds[k] + (exactQueryCount * 100)
+
+    sortedResults  = ((k, relevantDocIds[k]) for k in sorted(relevantDocIds, key=relevantDocIds.get, reverse=True)[:5])
+    for k,v in sortedResults:
+        print("\t",k, docIds[k], "\tRelevance Score: ", v)
+        #print("\t", docIds[docId], "\tOccurs: ", v, "times")
+
 
 
 
@@ -102,27 +143,37 @@ def main():
 
     # O(n^2)
     #  TODO Make this faster
+    print('Building index...')
     index = parseInvertedIndex()
     if DEBUG or False:
         for term in index:
             print(term, index[term])
 
     # O(n)
+    print('Building termIds...')
     termIds = parseTermIds()
     if DEBUG or False:
         for term in termIds:
             print(term, termIds[term])
 
     # O(n)
+    print('Building docIds...')
     docIds = parseDocIds()
     if DEBUG or False:
         for term in docIds:
             print(term, docIds[term])
 
+    # O(n)
+    print('Building docTexts...')
+    docTexts = parseDocs()
+    if DEBUG or False:
+        for docId in docTexts:
+            print(docId, docTexts[docId])
+
     query = _input("Enter query: ").strip().lower()
     while query != "":
         # O(n)
-        handleSearchQuery(query, termIds, docIds, index)
+        handleSearchQuery(query, termIds, docIds, index, docTexts)
         query = _input("Enter query: ").strip().lower()
 
 
